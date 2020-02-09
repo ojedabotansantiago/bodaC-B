b8f3af8cdf3ba03623cf1c224012f1fa4f646520
sojeda@funcionasi.es <sojeda@funcionasi.es>
2020-02-09 04:05:07 +0100

    refactor scss and try angular material

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -5,6 +5,7 @@ import { MatInputModule } from '@angular/material/input';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatRadioModule } from '@angular/material/radio';
 import { MatButtonModule } from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
 
 import { FormWeddingComponent } from './form-wedding/form-wedding.component';
 import { HeaderComponent } from './header/header.component';
@@ -23,8 +24,8 @@ import { PicturesComponent } from './pictures/pictures.component';
     MatInputModule,
     MatSliderModule,
     MatRadioModule,
-    MatButtonModule
-
+    MatButtonModule,
+    MatCardModule
   ],
   exports: [
     FormWeddingComponent,
