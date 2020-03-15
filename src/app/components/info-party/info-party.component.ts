c8fef34aa70c82c458e56e47e3ea99aedbea3685
sojeda@funcionasi.es <sojeda@funcionasi.es>
2020-03-15 14:09:34 +0100

    info party component

--- src/app/components/info-party/info-party.component.ts
+++ src/app/components/info-party/info-party.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-info-party',
@@ -6,10 +6,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./info-party.component.scss']
 })
 export class InfoPartyComponent implements OnInit {
-
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.5822099;
+  lng = -3.5816842;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 14,
+  };
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
   }
+  ngOnInit() {
+
+  }
+
 
 }
