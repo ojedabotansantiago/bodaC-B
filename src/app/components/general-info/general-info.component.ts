a5f6a615e275382bfe68d49e9b75a1adc6d309fb
sojeda@funcionasi.es <sojeda@funcionasi.es>
2020-03-15 12:59:23 +0100

    map config

--- src/app/components/general-info/general-info.component.ts
+++ src/app/components/general-info/general-info.component.ts
@@ -1,15 +1,33 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+
 
 @Component({
   selector: 'app-general-info',
   templateUrl: './general-info.component.html',
   styleUrls: ['./general-info.component.scss']
 })
-export class GeneralInfoComponent implements OnInit {
-
+export class GeneralInfoComponent implements OnInit, AfterViewInit  {
+  @ViewChild('mapContainer', { static: false }) gmap: ElementRef;
+  map: google.maps.Map;
+  lat = 40.4145318;
+  lng = -3.6910836;
+  coordinates = new google.maps.LatLng(this.lat, this.lng);
+  mapOptions: google.maps.MapOptions = {
+    center: this.coordinates,
+    zoom: 16,
+  };
+  marker = new google.maps.Marker({
+    position: this.coordinates,
+    map: this.map,
+  });
   constructor() { }
 
-  ngOnInit(): void {
+  ngAfterViewInit() {
+    this.map = new google.maps.Map(this.gmap.nativeElement, this.mapOptions);
+    this.marker.setMap(this.map);
+  }
+  ngOnInit() {
+
   }
 
 }
